7e219aed559f07fabafeeae2e373b2d73daf6e2f
PanOkurka <67430231+PanOkurka@users.noreply.github.com>
2021-03-21 15:29:18 +0100

    New Site update

---
+++ assets/js/animation.js
@@ -0,0 +1,39 @@
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["HTML", "PHP", "CSS", "NodeJS", "JS"]
+const typingDelay = 200;
+const arasingDelay = 100;
+const NewTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if(charIndex < textArray[textArrayIndex].length){
+        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay)
+    } else{
+        cursorSpan.classList.remove("typing");
+        setTimeout(erase, NewTextDelay)
+    }
+}
+
+function erase(){
+    if(charIndex > 0){
+        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0,charIndex-1);
+        charIndex--;
+        setTimeout(erase, arasingDelay)
+    } else{
+        cursorSpan.classList.remove("typing");
+        textArrayIndex++;
+        if(textArrayIndex>=textArray.length) textArrayIndex=0;
+        setTimeout(type, typingDelay + 1100)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    if(textArray.length) setTimeout(type, NewTextDelay + 250)
+})
